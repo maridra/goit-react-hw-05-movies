8c4e5c6f12362f2216e79a839ae6f52e1219fed0
Maryna-Dr <drapata.mb@gmail.com>
2022-11-13 16:10:58 +0200

    Added basename

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
 import MoviesWrapperCtx from 'context/MoviesCtx';
+import { BASE_PATH } from 'variabels/variabels';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={BASE_PATH}>
       <MoviesWrapperCtx>
         <App />
       </MoviesWrapperCtx>
---
+++ src/variabels/variabels.js
@@ -0,0 +1,1 @@
+export const BASE_PATH = '/goit-react-hw-05-movies';
