528a53a4d6e764583f1a814896a21ed3a77ee2c6
maridra <108903370+maridra@users.noreply.github.com>
2022-11-13 14:38:32 +0000

    Deploying to gh-pages from @ maridra/goit-react-hw-05-movies@bd84b65c86b2e0d559f16c42f505f3415b1d4811 🚀

---
+++ static/js/936.6c360f9c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{936:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(885),a=n(791),s=n(83),u=n(713),c="Home_title__L087v",f=n(184);function o(){var e=(0,a.useState)([]),t=(0,r.Z)(e,2),n=t[0],o=t[1];return(0,a.useEffect)((function(){(0,u.Df)().then((function(e){var t=e.results;o(t)}))}),[]),(0,f.jsxs)(f.Fragment,{children:[(0,f.jsx)("h2",{className:c,children:"Trending today"}),(0,f.jsx)(s.eW,{data:n})]})}}}]);
+//# sourceMappingURL=936.6c360f9c.chunk.js.map
